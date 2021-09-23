a0fae1f5cfbb67dc6a08ecc3376e19c3371086fd
Tyler Potts <tylerpottsdev@gmail.com>
2021-09-23 12:39:41 +0100

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+	"*.html"
+  ],
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+	fontFamily: {
+		sans: ["Fira sans", "sans-serif"]
+	}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
